bbac9c81f0a2f756726ab3c7e178a09ebf40d301
dhreyes <2590@holbertonschool.com>
2022-02-21 19:22:36 -0400

    Update with working config files

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
-//create new instance of discord bot
+
+
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+
+
+client.on("messageCreate", function(message) { 
+                                         
+});                                      
 
 client.login(config.BOT_TOKEN);
