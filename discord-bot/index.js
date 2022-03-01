aee21c4e3558a412789e461210efada79421b68c
dhreyes <2590@holbertonschool.com>
2022-03-01 00:00:25 -0400

    Add help and ping commands

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -4,9 +4,27 @@ const config = require('./config.json');
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
-
-client.on("messageCreate", function(message) { 
-                                         
-});                                      
-
 client.login(config.BOT_TOKEN);
+
+//Writing command handler for discord bot to use
+const prefix = "!";
+
+client.on("messageCreate", function(message) {
+
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    //Implementing a ping command
+    if (command === "ping") {
+        message.channel.send("Pong!");
+    }
+
+    //Implementing a help command
+    if (command === "help") {
+        message.channel.send("This is a help command");
+    }
+
+});
