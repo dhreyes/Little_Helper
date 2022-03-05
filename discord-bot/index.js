8426c9c7ef08245883ded9a9e6ae410a30fe4563
dhreyes <2590@holbertonschool.com>
2022-03-04 21:45:52 -0400

    Update with brand new commands for testing.

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -17,14 +17,29 @@ client.on("messageCreate", function(message) {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    //Implementing a ping command
+    //Implementing a ping command that tells you how long it took the bot to respond to the message from the user
+
     if (command === "ping") {
         message.channel.send("Pong!");
+        message.channel.send("It took " + message.createdTimestamp - message.createdTimestamp + "ms to send the message.");
     }
 
     //Implementing a help command
     if (command === "help") {
-        message.channel.send("This is a help command");
+        message.channel.send("The proper format for a command is !command argument1 argument2");
     }
 
+    //Implementing the ability for the bot to create a poll for users to vote on
+
+    if (command === "poll") {
+        const pollQuestion = args.join(" ");
+        message.channel.send(`@everyone ${pollQuestion}`);
+    }
+
+    //Implementing the ability for the bot to assign roles to new users that join the channel
+
+    if (command === "role") {
+        const role = args.join(' ');
+        message.member.roles.add(role);
+    }
 });
